872402ec3ae14247dafcb68350817dd692058f05
webjs-code <83772685+webjs-code@users.noreply.github.com>
2021-10-16 13:04:36 +0200

    Create footer-fw.component.ts

---
+++ formation-angular-12-2020/TP05-communication-composants/src/app/webApp/frameworks/footer-fw/footer-fw.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-fw',
+  templateUrl: './footer-fw.component.html',
+  styleUrls: ['./footer-fw.component.css']
+})
+export class FooterFWComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
